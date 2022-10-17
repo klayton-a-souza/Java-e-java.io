1b939b7bc4a7c6280bb2f1d0665f374e4cf7bedf
klayton-a-souza <klayton.al.souza@gmail.com>
2022-10-17 08:41:14 -0300

    Codigo 04: Usando java.util.Scanner

---
+++ Codigo/src/br/com/alura/java/io/teste/TesteLeitura2.java
@@ -0,0 +1,38 @@
+package br.com.alura.java.io.teste;
+
+import java.io.File;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class TesteLeitura2 {
+
+	public static void main(String[] args) throws Exception {
+
+		Scanner sc = new Scanner(new File("contas.csv"));
+
+		while (sc.hasNextLine()) {
+			String linha = sc.nextLine();
+
+			Scanner linhaScanner = new Scanner(linha);
+			linhaScanner.useLocale(Locale.US);
+			linhaScanner.useDelimiter(",");
+
+			String tipoConta = linhaScanner.next();
+			int agencia = linhaScanner.nextInt();
+			int numero = linhaScanner.nextInt();
+			String titular = linhaScanner.next();
+			double saldo = linhaScanner.nextDouble();
+
+			String valorFormatado = String.format(new Locale("pt", "BR"), "%s - %04d-%08d, %20s: %08.2f", tipoConta,
+					agencia, numero, titular, saldo);
+			System.out.println(valorFormatado);
+
+			linhaScanner.close();
+
+		}
+
+		sc.close();
+
+	}
+
+}
